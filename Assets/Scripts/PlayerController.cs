4d3525f1c133b8d92d2fcffcbfdd8bc3df4bf9f1
SplitPixl <splitpixl@gmail.com>
2019-01-24 16:25:01 -0800

    First

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float speed = 1;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float horiz = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horiz, 0, vert);
+
+        transform.Translate(movement * speed);
+    }
+}
