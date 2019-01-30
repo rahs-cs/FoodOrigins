4d22bf5586cbbc5725a24d2542d936ac44c22c0b
SplitPixl <splitpixl@gmail.com>
2019-01-30 15:54:55 -0800

    AnimatorMovement test with Asset Store assets

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
     public float speed = 1;
+    Animator aninmator;
 
     // Start is called before the first frame update
     void Start() {
-
+        aninmator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -18,12 +19,18 @@ public class PlayerController : MonoBehaviour {
 
         Vector2 control = new Vector2(horiz, vert);
         float angle = Vector2.SignedAngle(Vector2.up, control);
-        //        if(control.magnitude > 1) {
-        //            control.Normalize();
-        //        }
+        if(control.magnitude > 1) {
+            control.Normalize();
+        }
+
+        float magnitude = Vector2.ClampMagnitude(control, 1f).magnitude;
+
+        if (aninmator != null) {
+            aninmator.SetFloat("MoveSpeed", magnitude);
+        }
 
         if (control.magnitude > 0) {
-            Vector3 movement = new Vector3(0, 0, Vector2.ClampMagnitude(control, 1f).magnitude);
+            Vector3 movement = new Vector3(0, 0, magnitude);
             transform.eulerAngles = new Vector3(0, -angle, 0);
             transform.Translate(movement * speed);
         }
