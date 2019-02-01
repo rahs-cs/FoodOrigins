043be05d723dbc85dd4d85e96e08f55b60965a53
SplitPixl <splitpixl@gmail.com>
2019-01-31 16:13:17 -0800

    Collissions working

---
+++ Assets/Scripts/WallCollider.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallCollider : MonoBehaviour {
+    public bool isColliding = false;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        isColliding = true;
+        Debug.Log(collision);
+    }
+
+    private void OnCollisionStay(Collision collision) {
+        foreach (ContactPoint contact in collision.contacts) {
+            Debug.DrawRay(contact.point, contact.normal * 10, Color.yellow);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision) {
+        isColliding = false;
+        Debug.Log(collision);
+    }
+}
